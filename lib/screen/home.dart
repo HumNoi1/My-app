a5f9298140b12ca825220b20603d28a5dbaea6e7
HumNoi1 <framegg908@gmail.com>
2024-07-18 22:57:53 +0700

    Add Screen

---
+++ lib/screen/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Container(
+        color: Colors.purple[100],
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+        child: const Text(
+          "Hello World",
+          style: TextStyle(fontSize: 20, letterSpacing: 3),
+        ),
+      ),
+      Container(
+        color: Colors.purple[100],
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+        child: const Text(
+          "Hello World",
+          style: TextStyle(fontSize: 20, letterSpacing: 3),
+        ),
+      ),
+      Container(
+        color: Colors.purple[100],
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+        child: const Text(
+          "Hello World",
+          style: TextStyle(fontSize: 20, letterSpacing: 3),
+        ),
+      )
+    ]);
+  }
+}
